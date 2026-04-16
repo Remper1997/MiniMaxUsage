ddf00ae13e573e3ce43bcaf00c1e2ec85d158a42
Remper97 <dfrancescopaolo@yahoo.it>
2026-04-16 16:39:22 +0200

    feat: initialize Sparkle updater in AppDelegate

--- MiniMaxUsage/Sources/AppDelegate.swift
+++ MiniMaxUsage/Sources/AppDelegate.swift
@@ -4,12 +4,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var menuBarController: MenuBarController?
     private var apiService: ApiService?
     private var preferencesWindow: PreferencesWindow?
+    private var updateController: UpdateController?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
 
         apiService = ApiService()
         menuBarController = MenuBarController(apiService: apiService!)
+        updateController = UpdateController()
+        if SettingsHelper.autoUpdateEnabled {
+            updateController?.startAutomaticChecks()
+        }
 
         NotificationCenter.default.addObserver(
             forName: .showPreferences,
@@ -17,6 +22,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             queue: .main
         ) { [weak self] _ in
             self?.showPreferences()
+        }
+
+        NotificationCenter.default.addObserver(
+            forName: .enableAutoUpdate,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            self?.updateController?.startAutomaticChecks()
+        }
+
+        NotificationCenter.default.addObserver(
+            forName: .disableAutoUpdate,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            self?.updateController?.stopAutomaticChecks()
+        }
+
+        NotificationCenter.default.addObserver(
+            forName: .checkForUpdates,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            self?.updateController?.checkForUpdates()
+            SettingsHelper.lastUpdateCheck = Date()
         }
 
         if KeychainHelper.getAPIKey() == nil {
@@ -35,4 +65,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         preferencesWindow?.showWindow(nil)
         NSApp.activate(ignoringOtherApps: true)
     }
+}
+
+extension Notification.Name {
+    static let enableAutoUpdate = Notification.Name("enableAutoUpdate")
+    static let disableAutoUpdate = Notification.Name("disableAutoUpdate")
+    static let checkForUpdates = Notification.Name("checkForUpdates")
 }
